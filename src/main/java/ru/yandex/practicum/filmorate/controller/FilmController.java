a015d82257175c451e91f2b382d2ffc852f21055
MakkowniS <makkownis@yandex.ru>
2025-11-11 11:33:49 +1000

    Создать FilmController и исключение ValidationException

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,63 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class FilmController {
+
+    private Map<Integer, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> getFilms() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film createFilm(@RequestBody Film film) {
+        if (film.getName() == null || film.getName().isBlank()){
+            throw new ValidationException("Название не может быть пустым");
+        } else if (film.getDescription().length() > 200){
+            throw new ValidationException("Описание не может быть больше 200 символов");
+        } else if (film.getReleaseDate().isBefore(LocalDate.of(1895,12,28))){
+            throw new ValidationException("Дата релиза не может быть раньше 28.12.1895");
+        } else if (film.getDuration().toMinutes() < 0){
+            throw new ValidationException("Продолжительность не может быть отрицательной");
+        }
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film newFilm) {
+        if (newFilm.getId() == null){
+            throw new ValidationException("ID не может быть пустым");
+        }
+        Film oldFilm = films.get(newFilm.getId());
+        if (oldFilm == null){
+            throw new ValidationException("Фильма с ID: " + newFilm.getId() + " не найдено");
+        }
+        oldFilm.setName(newFilm.getName());
+        oldFilm.setDescription(newFilm.getDescription());
+        oldFilm.setReleaseDate(newFilm.getReleaseDate());
+        oldFilm.setDuration(newFilm.getDuration());
+
+        return oldFilm;
+    }
+
+    private int getNextId(){
+        int currentId = films.keySet().stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentId;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
+@EqualsAndHashCode(callSuper = false, of = "id")
 public class Film {
+    private Integer id;
+    private String name;
+    private String description;
+    @JsonFormat(shape =  JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate releaseDate;
+    private Duration duration;
 }
